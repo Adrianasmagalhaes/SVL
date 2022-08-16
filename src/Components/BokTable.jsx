f0ec9d9dc880c7d7678052047f67e3155b5d6cd5
anacarladsa <ana-carla2036@hotmail.com>
2022-08-16 20:41:33 -0300

    paginção

--- src/Components/BokTable.jsx
+++ src/Components/BokTable.jsx
@@ -1,58 +1,66 @@
 import React, { useEffect, useState } from 'react'
 import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
 
 export default function BookTable() {
-    let [posts, setPosts] = useState([])
+    //1-Criar o status que vai conter a lista da api
+    let [comments, setComments] = useState([])
 
-
+//2 - Criar o objeto colunas, no qual o dataFiel é a propriedade do objeto
     const columns = [{
         dataField: 'id',
-        text: 'Product ID'
+        text: ' ID'
     }, {
-        dataField: 'title',
-        text: 'Titulo'
+        dataField: 'name',
+        text: 'Nome'
     }, {
-        dataField: 'body',
-        text: 'Texto'
+        dataField: 'email',
+        text: 'Email'
+    },{
+        dataField:'body',
+        text:'Texto'
+
     }];
 
 
-    const Product = [{
-        id: 1,
-        name: 'Livro 1',
-        price: 10
-    },
-    {
-        id: 2,
-        name: 'Livro 2',
-        price: 50
-    },
-    {
-        id: 3,
-        name: 'Livro 3',
-        price: 100
-    }]
+    // const produto = [{
+    //     id: 1,
+    //     name: 'Livro 1',
+    //     price: 10
+    // },
+    // {
+    //     id: 2,
+    //     name: 'Livro 2',
+    //     price: 50
+    // },
+    // {
+    //     id: 3,
+    //     name: 'Livro 3',
+    //     price: 100
+    // }]
 
-    const getPosts = () => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
+    const getComments = () => {
+        fetch('https://jsonplaceholder.typicode.com/comments')
             .then(Response => Response.json())
             .then(data => {
-                setPosts(data)
-                console.log('posts', data)
+                setComments(data)
+                console.log('comments', data)
             
             })
 
     }
     
         useEffect(() => {
-        getPosts();
+        getComments();
     
     }, [])
 
 
     return (
         <div>
-            <BootstrapTable keyField='id' data={posts} columns={columns} />
+            <BootstrapTable keyField='id' data={comments} columns={columns}  pagination={ paginationFactory()} />
+            
+        
         </div>
     )
 }
